976ad52ae7b3ef015b7b903fff21461e1ffab658
Shio <inocentvicent@gmail.com>
2016-04-27 16:35:25 +0300

    clean codes
    removed ..... unnecessary functions

--- admin/forms/building/building.php
+++ admin/forms/building/building.php
@@ -1,185 +1,48 @@
-<div class="container">
-	
-	<div class="panel panel-primary">
-		<form class="form-horizontal well span4" action="forms/building/controller.php?action=add" method="POST">
-			<form class="form-horizontal well span4" action="#.php" method="POST">
-				<fieldset>
-					<legend>Add New Asset</legend>
-					<p>
+
+
+<form class="form-horizontal well span4" action="forms/building/controller.php?action=add" method="POST">
+	<fieldset>
+		<legend>Add New Asset</legend>
+		<p>
+			<div class="form-group">
+				<div class="col-md-8">
+					<label class="col-md-4 control-label" for="name">Asset Name</label>
+					<div class="col-md-8">
+						<input class="form-control input-sm" id="name" name="name" placeholder="Asset Name" type="text" value="">
+					</div>
+				</div>
+			</div>
+			
+			
+			<div class="form-group">
+				<div class="col-md-8">
+					<label class="col-md-4 control-label" for="type">Asset Type</label>
+					<div class="col-md-8">
+						<input class="form-control input-sm" id="type" name="type" placeholder="Asset Type" type="text" value="">
+					</div>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<div class="col-md-8">
+					<label class="col-md-4 control-label" for="manufacturername">rooms</label>
+					<div class="col-md-8">
+						<input class="form-control input-sm" id="room" name="room" placeholder="total rooms" type="text" value="">
+						
+						
+						<br>
 						<div class="form-group">
 							<div class="col-md-8">
-								<label class="col-md-4 control-label" for="name">Asset Name</label>
+								<label class="col-md-4 control-label" for= "idno"></label>
 								<div class="col-md-8">
-									<input class="form-control input-sm" id="name" name="name" placeholder="Asset Name" type="text" value="">
-								</div>
-							</div>
-						</div>
-						
-						
-						<div class="form-group">
-							<div class="col-md-8">
-								<label class="col-md-4 control-label" for="type">Asset Type</label>
-								<div class="col-md-8">
-									<input class="form-control input-sm" id="type" name="type" placeholder="Asset Type" type="text" value="">
-								</div>
-							</div>
-						</div>
-						
-						
-						
-						
-						
-						<div class="form-group">
-							<div class="col-md-8">
-								<label class="col-md-4 control-label" for="manufacturername">rooms</label>
-								<div class="col-md-8">
-									<input class="form-control input-sm" id="room" name="room" placeholder="total rooms" type="text" value="">
-									
-									<!--
-									<input class="form-control input-sm" id="manufacturername" name="manufacturername" placeholder="Manufacturer Name" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="modelnumber">Model Number</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="modelnumber" name="modelnumber" placeholder="Model Number" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="suppliername">Supplier</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="suppliername" name="suppliername" placeholder="Supplier Name" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="departmentname">Department</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="departmentname" name="departmentname" placeholder="Department Name" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="serialnumber">Serial Number</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="serialnumber" name="serialnumber" placeholder="Serial Number" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="purchaseddate">Purchased Date</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="purchaseddate" name="purchaseddate" placeholder= "Purchased Date" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="purchasedcost">Purchased Cost</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="purchasedcost" name="purchasedcost" placeholder="Purchased Cost" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="warrantystart">Warranty Start</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="warrantystart" name="warrantystart" placeholder="Warranty Start" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="warrantyfinish">Warranty Finish</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="warrantyfinish" name="warrantyfinish" placeholder="Warranty Finish " type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="comments">Comments</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="comments" name="comments" placeholder="Comments" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for="status">Status</label>
-									<div class="col-md-8">
-									<input name="deptid" type="hidden" value="">
-									<input class="form-control input-sm" id="status" name="status" placeholder="Status" type="text" value="">
-									</div>
-									</div>
-									</div>
-									
-									
-									
-									<label class="control-label">Select File</label>
-									<input id="input-7" name="input7[]" multiple type="file" class="file file-loading" data-allowed-file-extensions='["csv", "txt"]'>
-									
-									
-									
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for= "idno"></label>
-									<div class="col-md-8">
-									<button class="btn btn-default" name="save" type="submit" ><span class="glyphicon glyphicon-floppy-save"></span> Save</button>
-									</div>
-									</div>
-									</div>  -->
-									
-									
-									
-									<br>
-									<div class="form-group">
-									<div class="col-md-8">
-									<label class="col-md-4 control-label" for= "idno"></label>
-									<div class="col-md-8">
 									<button class="btn btn-default" name="save" type="save" ><span class="glyphicon glyphicon-floppy-save"></span> Save</button>
-									</div>
-									</div>
-									</div>  
-									</fieldset> 
-									</form>
-									</div> <!--End of container-->	
-									
-									</div>
-									
-																		
+								</div>
+							</div>
+						</div>  
+					</fieldset> 
+				</form>
+			</div> <!--End of container-->	
+			
+		</div>
+		
+		
